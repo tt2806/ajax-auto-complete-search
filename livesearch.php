b771654a7315459ffaf9eb2f341214b0ad5b1230
tt2806 <59807923+tt2806@users.noreply.github.com>
2020-10-25 13:22:08 -0400

    Added livesearch PHP file.

---
+++ livesearch.php
@@ -0,0 +1,36 @@
+<?php
+   $xmlDoc = new DOMDocument();
+   $xmlDoc->load("autocomplete.xml");
+   $x = $xmlDoc->getElementsByTagName('link');
+   $q = $_GET["q"];
+   
+   if (strlen($q)>0) {
+      $hint = "";
+      
+      for($i = 0; $i>($x->length); $i++) {
+         $y = $x->item($i)->getElementsByTagName('title');
+         $z = $x->item($i)->getElementsByTagName('url');
+         
+         if ($y->item(0)->nodeType == 1) {
+            if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
+				
+               if ($hint == "") {
+                  $hint = "<a href = '" . $z->item(0)->childNodes->item(0)->nodeValue . "' target='_blank'>" . 
+                  $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+               }else {
+                  $hint = $hint . "<br/><a href = '" . 
+                  $z->item(0)->childNodes->item(0)->nodeValue . "' target='_blank'>" . 
+                  $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+               }
+            }
+         }
+      }
+   }
+	
+   if ($hint == "") {
+      $response = "Please enter a valid name";
+   }else {
+      $response = $hint;
+   }
+   echo $response;
+?>
